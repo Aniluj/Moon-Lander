ad11b368f97641314c7c0744b83a966747c4d04d
Aniluj <julianvolonterio@gmail.com>
2017-06-12 07:31:34 -0300

    Script de movimiento; nuevas carpetas
    Agregado un script de movimiento a la nave, carpetas creadas para una
    mejor organización y modificación en los drag del RigidBody de la nave
    para generar un parecido lo más significativo posible al juego original.

---
+++ Clase 10/Assets/Scripts/SpaceShipMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShipMovement : MonoBehaviour {
+
+	private Rigidbody2D spaceShip;
+	private float horizontalAxis;
+	private float verticalAxis;
+	public float velocityInY = 0f;
+	public float velocityOfRotation = 0f;
+
+	void Awake(){
+		spaceShip = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update () {
+		horizontalAxis = Input.GetAxisRaw ("Horizontal");
+		verticalAxis = Input.GetAxis ("Vertical");
+	}
+
+	void FixedUpdate (){
+		spaceShip.AddTorque (velocityOfRotation * horizontalAxis, ForceMode2D.Force);
+		spaceShip.AddRelativeForce (Vector2.up * velocityInY * verticalAxis, ForceMode2D.Force);
+	}
+}
